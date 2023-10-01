4f3a952f16f4620a0ca85ad1280073f0685e7e4b
Viktor Svider <viktor1svider@gmail.com>
2023-10-01 12:47:32 +0300

    add multiple posts view

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,4 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Freddie'}
-    return render_template('index.html', title="Microblog", user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in London'
+        },
+        {
+            'author': {'username': 'Steven'},
+            'body': 'Matrix 4 review'
+        }
+            
+    ]
+    return render_template('index.html', title="Microblog", user=user, posts=posts)
